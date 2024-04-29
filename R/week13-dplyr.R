e06ded13da2a72198a2ca48a9b6545f2930dd414
lisette-18 <horne146@umn.edu>
2024-04-28 20:24:36 -0500

    Data Import and Cleaning

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -5,5 +5,19 @@ library(RMariaDB)
 library(tidyverse)
 
 #Data Import and Cleaning
+conn <- dbConnect(MariaDB(), #use dbConnect as required by class
+                  user="horne146",
+                  password=key_get("latis-mysql","horne146"),
+                  host="mysql-prod5.oit.umn.edu",
+                  port=3306,
+                  ssl.ca = "mysql_hotel_umn_20220728_interm.cer")
 
-#Analysis
+show_dbs <- dbGetQuery(conn, "SHOW DATABASES;") #show databases present
+dbExecute(conn, "USE cla_tntlab;")
+
+employees_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_employees;") %>% 
+  as_tibble() #using dbGetQuery and as_tibble to create tibble
+testscores_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_testscores;") %>%
+  as_tibble() #using dbGetQuery and as_tibble to create tibble
+offices_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_offices;") %>%
+  as_tibble() #using dbGetQuery and as_tibble to create tibble
