d0c2d8f64c7fd19a36b1f3c066f95528f27e7ab2
lisette-18 <horne146@umn.edu>
2024-04-28 20:25:03 -0500

    Write CSV files

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -21,3 +21,14 @@ testscores_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_testscores;") %>%
   as_tibble() #using dbGetQuery and as_tibble to create tibble
 offices_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_offices;") %>%
   as_tibble() #using dbGetQuery and as_tibble to create tibble
+
+##CSV Files
+write_csv(employees_tbl, "../data/employees.csv") #use write_csv in order to convert tibbles to csv files to be read in
+write_csv(testcores_tbl, "../data/testscores.csv") #use write_csv in order to convert tibbles to csv files to be read in
+write_csv(offices_tbl, "../data/offices.csv") #use write_csv in order to convert tibbles to csv files to be read in
+
+week13_tbl <- tibble(employees_tbl %>% #use tibble() to create as a tibble
+                       inner_join(testscores_tbl, by = "employee_id") %>% #inner_join to connect testscores to the employees tbl
+                       full_join(offices_tbl, by = "city")) #use full_join to combine based on the previous two databases being combined
+
+write_csv(week13_tbl, "../out/week13.csv")
